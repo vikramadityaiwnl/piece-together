39f25965c21487082b83c1f217587f138e1f9d66
Vikramaditya Dhumal <dhumalvikramaditya287@gmail.com>
2024-12-12 11:28:20 +0530

    initial commit

---
+++ src/createPost.tsx
@@ -0,0 +1,29 @@
+import { Devvit } from '@devvit/public-api';
+
+// Configure Devvit's plugins
+Devvit.configure({
+  redditAPI: true,
+});
+
+// Adds a new menu item to the subreddit allowing to create a new post
+Devvit.addMenuItem({
+  label: 'Create New Jigsaw Puzzle',
+  location: 'subreddit',
+  forUserType: 'moderator',
+  onPress: async (_event, context) => {
+    const { reddit, ui } = context;
+    const subreddit = await reddit.getCurrentSubreddit();
+    const post = await reddit.submitPost({
+      title: 'Jigsaw Puzzle',
+      subredditName: subreddit.name,
+      // The preview appears while the post loads
+      preview: (
+        <vstack height="100%" width="100%" alignment="middle center">
+          <text size="large">Loading ...</text>
+        </vstack>
+      ),
+    });
+    ui.showToast({ text: 'Post created!' });
+    ui.navigateTo(post);
+  },
+});
